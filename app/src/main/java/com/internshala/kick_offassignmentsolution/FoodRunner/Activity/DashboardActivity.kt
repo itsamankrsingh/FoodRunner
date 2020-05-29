cf6c2ebca1da32f7646b4b8fcf459942b1468eac
itsamankrsingh <itsamankrsingh@gmail.com>
2020-05-29 20:01:30 +0530

    fourth commit added title to different fragment and functionality to logout

--- app/src/main/java/com/internshala/kick_offassignmentsolution/FoodRunner/Activity/DashboardActivity.kt
+++ app/src/main/java/com/internshala/kick_offassignmentsolution/FoodRunner/Activity/DashboardActivity.kt
@@ -1,18 +1,21 @@
 package com.internshala.kick_offassignmentsolution.FoodRunner.Activity
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.FrameLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import com.internshala.kick_offassignmentsolution.FoodRunner.Fragment.HomeFragment
+import com.internshala.kick_offassignmentsolution.FoodRunner.Fragment.*
 import com.internshala.kick_offassignmentsolution.R
 
 class DashboardActivity : AppCompatActivity() {
@@ -24,6 +27,7 @@ class DashboardActivity : AppCompatActivity() {
     lateinit var toolbar: Toolbar
     lateinit var frameLayout: FrameLayout
     lateinit var navigationView: NavigationView
+    var previousMenuItem: MenuItem? = null
 
     /*Life-cycle method*/
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +41,8 @@ class DashboardActivity : AppCompatActivity() {
         navigationView = findViewById(R.id.navigationViewDashboard)
         setSupportToolBar()
 
+        openHome()
+
         val actionBarDrawerToggle = ActionBarDrawerToggle(
             this@DashboardActivity,
             drawerLayout,
@@ -49,78 +55,83 @@ class DashboardActivity : AppCompatActivity() {
 
         /*setting up click listener for menu items in drawer*/
         navigationView.setNavigationItemSelectedListener {
+
+            /*checkingif previous item is checked or not*/
+            if (previousMenuItem != null) {
+                previousMenuItem?.isChecked = false
+            }
+
+            it?.isCheckable = true
+            it?.isChecked = true
+            previousMenuItem = it
+
             when (it.itemId) {
                 R.id.menuHomePage -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.frame, HomeFragment())
-                        .commit()
+                    openHome()
                     drawerLayout.closeDrawers()
                 }
                 R.id.menuProfilePage -> {
-                    Toast.makeText(this@DashboardActivity, "Clicked on Profile", Toast.LENGTH_SHORT)
-                        .show()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame, ProfileFragment())
+                        .commit()
+                    supportActionBar?.title = "My Profile"
+                    drawerLayout.closeDrawers()
                 }
                 R.id.menuFavoritesPage -> {
-                    Toast.makeText(
-                        this@DashboardActivity,
-                        "Clicked on Favorite",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame, FavoriteFragment())
+                        .commit()
+                    supportActionBar?.title = "Favorite Restaurants"
+                    drawerLayout.closeDrawers()
                 }
                 R.id.menuOrderHistory -> {
-                    Toast.makeText(this@DashboardActivity, "Clicked on Order", Toast.LENGTH_SHORT)
-                        .show()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame, OrderHistoryFragment())
+                        .commit()
+                    supportActionBar?.title = "My Previous Orders"
+                    drawerLayout.closeDrawers()
                 }
                 R.id.menuFaqPage -> {
-                    Toast.makeText(this@DashboardActivity, "Clicked on FAQ", Toast.LENGTH_SHORT)
-                        .show()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame, FaqFragment())
+                        .commit()
+                    supportActionBar?.title = "Frequently Asked Questions"
+                    drawerLayout.closeDrawers()
                 }
+                //add dialog alert box to show logout message
                 R.id.menuLogout -> {
-                    Toast.makeText(this@DashboardActivity, "Clicked on Logout", Toast.LENGTH_SHORT)
-                        .show()
+                    var builder = AlertDialog.Builder(this@DashboardActivity)
+                    builder.setTitle("Confirmation")
+                    builder.setMessage("Are you sure you want to exit?")
+                    builder.setPositiveButton(
+                        "Yes",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            Toast.makeText(
+                                this@DashboardActivity,
+                                "You are Logged Out!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            startActivity(Intent(this@DashboardActivity, LoginActivity::class.java))
+                            finish()
+                        })
+                    builder.setNegativeButton(
+                        "Cancel",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            Toast.makeText(
+                                this@DashboardActivity,
+                                "You are canceled the action!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            it.isChecked = false
+                            openHome()
+                            drawerLayout.closeDrawers()
+                        })
+                    builder.create()
+                    builder.show()
                 }
             }
             return@setNavigationItemSelectedListener true
         }
-
-        /*Initialising the textview and linking it with the textview created in XML*/
-        txtData = findViewById(R.id.txtData)
-
-        /*Checking whether any data was received through the intent or not*/
-        if (intent != null) {
-
-            /*Fetching the details from the intent*/
-            val details = intent.getBundleExtra("details")
-
-            /*Getting the value of data from the bundle object*/
-            val data = details.getString("data")
-
-            /*Checking the location from which data was sent*/
-            if (data == "login") {
-                /*Creating the text to be displayed*/
-                val text = "Mobile Number : ${details.getString("mobile")} \n " +
-                        "Password : ${details.getString("password")}"
-                txtData.text = text
-            }
-
-            if (data == "register") {
-                val text = " Name : ${details.getString("name")} \n " +
-                        "Mobile Number : ${details.getString("mobile")} \n " +
-                        "Password : ${details.getString("password")} \n " +
-                        "Address: ${details.getString("address")}"
-                txtData.text = text
-            }
-
-            if (data == "forgot") {
-                val text = "Mobile Number : ${details.getString("mobile")} \n " +
-                        "Email : ${details.getString("email")}"
-                txtData.text = text
-            }
-
-        } else {
-            /*No data was received through the intent*/
-            txtData.text = "No data received!!"
-        }
     }
+
 
     fun setSupportToolBar() {
         setSupportActionBar(toolbar)
@@ -135,5 +146,24 @@ class DashboardActivity : AppCompatActivity() {
             drawerLayout.openDrawer(GravityCompat.START)
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun openHome() {
+        val fragment = HomeFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame, fragment)
+        transaction.commit()
+        supportActionBar?.title = "All Restaurants"
+        navigationView.setCheckedItem(R.id.menuHomePage)
+    }
+
+    override fun onBackPressed() {
+        val frag = supportFragmentManager.findFragmentById(R.id.frame)
+
+        when (frag) {
+            !is HomeFragment -> openHome()
+
+            else -> super.onBackPressed()
+        }
     }
 }
---
+++ app/src/main/java/com/internshala/kick_offassignmentsolution/FoodRunner/Fragment/ProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.internshala.kick_offassignmentsolution.FoodRunner.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.internshala.kick_offassignmentsolution.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view=inflater.inflate(R.layout.fragment_profile, container, false)
+        return view
+    }
+
+}
